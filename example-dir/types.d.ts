5c25560c786ae0e547bc3005c1089f4e1a8e3336
Shibo <shibosoftwaredev@gmail.com>
2024-12-16 21:22:03 +0200

    format and returned type files

--- example-dir/types.d.ts
+++ example-dir/types.d.ts
@@ -1,11 +1,11 @@
-// declare module "@tsci/seveibar.red-led" {
-//   export function useRedLed(name: string): any
-// }
+declare module "@tsci/seveibar.red-led" {
+  export function useRedLed(name: string): any
+}
 
-// declare module "@tsci/seveibar.push-button" {
-//   export function usePushButton(name: string): any
-// }
+declare module "@tsci/seveibar.push-button" {
+  export function usePushButton(name: string): any
+}
 
-// declare module "@tsci/seveibar.smd-usb-c" {
-//   export function useUsbC(name: string): any
-// }
+declare module "@tsci/seveibar.smd-usb-c" {
+  export function useUsbC(name: string): any
+}
