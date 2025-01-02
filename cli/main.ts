e9049d95ca450ef8d8c431cbf73ff071f2fd591b
seveibar <seveibar@gmail.com>
2025-01-02 00:22:33 -0800

    login working

--- cli/main.ts
+++ cli/main.ts
@@ -3,16 +3,29 @@ import { Command } from "commander"
 import { registerDev } from "./dev/register"
 import { registerAuthLogin } from "./auth/login/register"
 import { registerAuthLogout } from "./auth/logout/register"
+import { registerAuth } from "./auth/register"
+import { registerConfig } from "./config/register"
+import { registerConfigPrint } from "./config/print/register"
+import { perfectCli } from "perfect-cli"
 
 const program = new Command()
 
 program
-  .name("snippets")
+  .name("tsci")
   .description("CLI for developing tscircuit snippets")
   .version("1.0.0")
 
 registerDev(program)
+
+registerAuth(program)
 registerAuthLogin(program)
 registerAuthLogout(program)
 
-program.parse()
+registerConfig(program)
+registerConfigPrint(program)
+
+if (process.argv.length === 2) {
+  perfectCli(program, process.argv)
+} else {
+  program.parse()
+}
--- lib/cli-config/index.ts
+++ lib/cli-config/index.ts
@@ -4,8 +4,13 @@ import type { TypedConfigstore } from "./TypedConfigStore"
 export interface CliConfig {
   sessionToken?: string
   githubUsername?: string
+  registryApiUrl?: string
 }
 
 export const cliConfig: TypedConfigstore<CliConfig> = new Configstore(
   "tscircuit",
 )
+
+export const getRegistryApiUrl = (): string => {
+  return cliConfig.get("registryApiUrl") ?? "https://registry-api.tscircuit.com"
+}
