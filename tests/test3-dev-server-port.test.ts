8d637179931b3bc4526ec30de9dd4ece3afe9bbd
Krushna Kanta Rout <129386740+krushnarout@users.noreply.github.com>
2025-01-23 23:30:06 +0530

    fix: format check

--- tests/test3-dev-server-port.test.ts
+++ tests/test3-dev-server-port.test.ts
@@ -17,14 +17,14 @@ test("test3 dev server port handling", async () => {
   })
 
   const server = http.createServer(() => {}).listen(3020)
-  
+
   const devServer = new DevServer({
     port: 3021,
     componentFilePath: `${tempDirPath}/snippet.tsx`,
   })
   await devServer.start()
 
-  await new Promise(resolve => setTimeout(resolve, 1000))
+  await new Promise((resolve) => setTimeout(resolve, 1000))
 
   const res = await fetch(`http://localhost:3021`)
   expect(res.status).toBe(200)
