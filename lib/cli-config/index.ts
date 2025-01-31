0a94c59ab455ecb5ced114a5409225b9b9fae3d3
K Om Senapati <komnoob123@gmail.com>
2025-01-31 08:25:30 +0530

    feat: get githubUsername from session token (#42)

--- lib/cli-config/index.ts
+++ lib/cli-config/index.ts
@@ -1,5 +1,6 @@
 import Configstore from "configstore"
 import type { TypedConfigstore } from "./TypedConfigStore"
+import { jwtDecode } from "jwt-decode"
 
 export interface CliConfig {
   sessionToken?: string
@@ -10,6 +11,14 @@ export interface CliConfig {
 export const cliConfig: TypedConfigstore<CliConfig> = new Configstore(
   "tscircuit",
 )
+
+export const setSessionToken = (token: string) => {
+  cliConfig.set("sessionToken", token)
+  const decoded = jwtDecode<{
+    github_username: string
+  }>(token)
+  cliConfig.set("githubUsername", decoded.github_username)
+}
 
 export const getRegistryApiUrl = (): string => {
   return cliConfig.get("registryApiUrl") ?? "https://registry-api.tscircuit.com"
