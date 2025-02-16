04e11bd84fe91a3a72192102c699668654e2bcdb
Arnav K <arnavkaushal09@gmail.com>
2025-02-16 18:01:22 +0000

    better

--- tests/test1-dev-server-basic.test.ts
+++ tests/test1-dev-server-basic.test.ts
@@ -26,7 +26,7 @@ test("test1 basic dev server filesystem watching", async () => {
 
   const { file_list } = await devServer.fsKy.get("api/files/list").json()
 
-  expect(file_list.map((f) => f.file_path).sort()).toContainAllValues([
+  expect(file_list.map((f) => f.file_path).sort()).toContainValues([
     "entrypoint.tsx",
     "manual-edits.json",
     "snippet.tsx",
