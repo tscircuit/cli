9f920090fee8751568c8204e715c9d1179c6e6f2
seveibar <seveibar@gmail.com>
2025-02-18 18:15:04 -0800

    format

--- tsup.config.js
+++ tsup.config.js
@@ -1,10 +1,10 @@
-import { defineConfig } from 'tsup'
+import { defineConfig } from "tsup"
 
 export default defineConfig({
-  entry: ['cli/main.ts'],
-  format: ['cjs'],
-  platform: 'node',
-  sourcemap: 'inline',
+  entry: ["cli/main.ts"],
+  format: ["cjs"],
+  platform: "node",
+  sourcemap: "inline",
   external: [],
   bundle: true,
   // Enable this to see the bundle size
