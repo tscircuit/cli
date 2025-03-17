480526cbe2812eb7b77cc782bdc8ee0a9c2dc41b
Rishabh Gupta <38923768+imrishabh18@users.noreply.github.com>
2025-03-17 18:48:12 +0530

    refactor the export command (#106)
    * refactor the export command
    
    * format

--- tests/cli/export/export.test.ts
+++ tests/cli/export/export.test.ts
@@ -108,3 +108,29 @@ NET: C1_pos
 "
 `)
 })
+
+test("export circuit-json", async () => {
+  const { tmpDir, runCommand } = await getCliTestFixture()
+  const circuitPath = path.join(tmpDir, "test-circuit.tsx")
+
+  await writeFile(circuitPath, circuitCode)
+
+  const { stdout, stderr } = await runCommand(
+    `tsci export ${circuitPath} -f circuit-json`,
+  )
+  expect(stderr).toBe("")
+
+  console.log(stdout, "stdout")
+
+  const circuitJson = await readFile(
+    path.join(tmpDir, "test-circuit.circuit.json"),
+    "utf-8",
+  )
+  const source_component = JSON.parse(circuitJson).filter(
+    (component: any) => component.type === "source_component",
+  )
+
+  expect(source_component.length).toBe(2)
+  expect(source_component[0].name).toBe("R1")
+  expect(source_component[1].name).toBe("C1")
+})
