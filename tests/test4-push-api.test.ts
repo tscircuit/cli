82a9a7791ea23053169f916538c7cfced5df6029
Rishabh Gupta <belikerishabh@gmail.com>
2025-03-25 13:26:52 +0530

    type fix

--- tests/test4-push-api.test.ts
+++ tests/test4-push-api.test.ts
@@ -8,9 +8,13 @@ test("should fail if no entrypoint file is found", async () => {
   try {
     await runCommand("tsci push")
   } catch (e) {
-    expect(e.message).toContain(
-      "No entrypoint found. Run 'tsci init' to bootstrap a basic project.",
-    )
+    if (e instanceof Error) {
+      expect(e.message).toContain(
+        "No entrypoint found. Run 'tsci init' to bootstrap a basic project.",
+      )
+    } else {
+      throw e
+    }
   }
 })
 
