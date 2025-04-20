367b39b5fe92cac3074f54b2cf8b759fc1ad9bea
Arnav K <arnavkaushal09@gmail.com>
2025-04-21 00:33:33 +0530

    feat(cli): add upgrade command to check for CLI updates (#176)
    * feat(cli): add upgrade command to check for CLI updates
    
    This commit introduces a new `upgrade` command that allows users to check for the latest version of the CLI. If the user is already on the latest version, a message is displayed indicating no update is needed. This enhances the CLI by providing a built-in mechanism for version management.
    
    * chore
    
    * chore

---
+++ cli/upgrade/register.ts
@@ -0,0 +1,17 @@
+import type { Command } from "commander"
+import chalk from "chalk"
+import { checkForTsciUpdates } from "lib/shared/check-for-cli-update"
+
+export function registerUpgradeCommand(program: Command) {
+  program
+    .command("upgrade")
+    .description("Upgrade CLI to the latest version")
+    .action(async () => {
+      const isUpdated = await checkForTsciUpdates()
+      if (!isUpdated) {
+        console.log(
+          chalk.green("You are already using the latest version of tsci."),
+        )
+      }
+    })
+}
