af255e47d8558552e176924b7f5be6dc28a5e8a5
Severin Ibarluzea <seveibar@gmail.com>
2025-06-03 11:21:29 -0700

    Improve error reporting for API requests (#210)
    * Show request body in API error
    
    * Improve error messages with API error details
    
    * fix type error for api error object

--- lib/registry-api/get-ky.ts
+++ lib/registry-api/get-ky.ts
@@ -8,11 +8,27 @@ export const prettyResponseErrorHook: AfterResponseHook = async (
 ) => {
   if (!response.ok) {
     try {
-      const errorData = await response.json()
+      const errorData = (await response.json()) as any
+
+      let requestBody = ""
+      try {
+        requestBody = await _request.clone().text()
+      } catch {
+        // ignore errors cloning request body
+      }
+
+      const apiError = errorData?.error
+      const errorString = apiError
+        ? `\n${apiError.error_code}: ${apiError.message}`
+        : ""
+
       throw new Error(
         `FAIL [${response.status}]: ${_request.method} ${
           new URL(_request.url).pathname
-        } \n\n ${JSON.stringify(errorData, null, 2)}`,
+        }` +
+          errorString +
+          (requestBody ? `\n\nRequest Body:\n${requestBody}` : "") +
+          `\n\n${JSON.stringify(errorData, null, 2)}`,
       )
     } catch (e) {
       //ignore, allow the error to be thrown
