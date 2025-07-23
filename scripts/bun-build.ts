7263fa938e7ad3d15822f928096ce002870bfddf
seveibar <seveibar@gmail.com>
2025-07-23 13:37:09 -0700

    add explicit script for bun build to reduce bundling issue

---
+++ scripts/bun-build.ts
@@ -0,0 +1,23 @@
+import { extname, basename } from "node:path"
+
+const result = await Bun.build({
+  entrypoints: ["./cli/main.ts"],
+  target: "node",
+  outdir: "./dist",
+  external: ["@tscircuit/*", "looks-same", "tscircuit", "circuit-to-svg"],
+})
+
+const { outputs, success } = result
+
+if (!success) {
+  console.error("Build failed", result.logs)
+  process.exit(1)
+}
+
+for (const output of outputs) {
+  console.log(
+    `${basename(output.path)} ${(output.size / 1024 / 1024).toFixed(2)} MB`,
+  )
+}
+
+export {}
