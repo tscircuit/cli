711e68e3220a92570626b704eb7d2f11fd583206
Severin Ibarluzea <seveibar@gmail.com>
2025-07-23 17:34:20 -0700

    Externalize the majority of dependencies, including typescript and deps provided by "tscircuit" (#296)
    * add explicit script for bun build to reduce bundling issue
    
    * wip
    
    * don't bundle typescript
    
    * allow bundling of runframe
    
    * install tscircuit in workflow
    
    * avoid bin conflict

---
+++ scripts/bun-build.ts
@@ -0,0 +1,37 @@
+import { extname, basename } from "node:path"
+// @ts-ignore
+import tscircuitPackageJson from "tscircuit/package.json"
+
+const tscircuitPackageJsonDeps = Object.keys(tscircuitPackageJson.dependencies)
+
+const ALLOW_BUNDLING = ["@tscircuit/runframe"]
+
+const result = await Bun.build({
+  entrypoints: ["./cli/main.ts"],
+  target: "node",
+  outdir: "./dist",
+  external: [
+    ...tscircuitPackageJsonDeps.filter((dep) => !ALLOW_BUNDLING.includes(dep)),
+    "zod",
+    "looks-same",
+    "tscircuit",
+    "typescript",
+    "circuit-to-svg",
+    "@types/*",
+  ],
+})
+
+const { outputs, success } = result
+
+if (!success) {
+  console.error("Build failed", result.logs)
+  process.exit(1)
+}
+
+for (const output of outputs) {
+  console.log(
+    `${basename(output.path)} ${(output.size / 1024 / 1024).toFixed(2)} MB`,
+  )
+}
+
+export {}
