ba228cfe8c1f45ff3539fb80c6988b32863f6e07
Severin Ibarluzea <seve700@gmail.com>
2025-09-25 09:39:02 -0700

    Support building index entrypoint when circuits absent

--- cli/build/get-build-entrypoints.ts
+++ cli/build/get-build-entrypoints.ts
@@ -17,29 +17,48 @@ export async function getBuildEntrypoints({
 }> {
   const resolvedRoot = path.resolve(rootDir)
 
-  if (fileOrDir) {
-    const resolved = path.resolve(resolvedRoot, fileOrDir)
-    if (fs.existsSync(resolved) && fs.statSync(resolved).isDirectory()) {
-      const projectDir = resolved
-      const files = globbySync(["**/*.board.tsx", "**/*.circuit.tsx"], {
-        cwd: projectDir,
-        ignore: DEFAULT_IGNORED_PATTERNS,
-      })
+  const buildFromProjectDir = async (projectDir: string) => {
+    const files = globbySync(["**/*.board.tsx", "**/*.circuit.tsx"], {
+      cwd: projectDir,
+      ignore: DEFAULT_IGNORED_PATTERNS,
+    })
+
+    if (files.length > 0) {
       return {
         projectDir,
         circuitFiles: files.map((f) => path.join(projectDir, f)),
       }
     }
+
+    const mainEntrypoint = await getEntrypoint({
+      projectDir,
+      onSuccess: () => undefined,
+      onError: () => undefined,
+    })
+
+    if (mainEntrypoint) {
+      return {
+        projectDir,
+        mainEntrypoint,
+        circuitFiles: [mainEntrypoint],
+      }
+    }
+
+    return {
+      projectDir,
+      circuitFiles: [],
+    }
+  }
+
+  if (fileOrDir) {
+    const resolved = path.resolve(resolvedRoot, fileOrDir)
+    if (fs.existsSync(resolved) && fs.statSync(resolved).isDirectory()) {
+      const projectDir = resolved
+      return buildFromProjectDir(projectDir)
+    }
     return { projectDir: path.dirname(resolved), circuitFiles: [resolved] }
   }
 
   const projectDir = resolvedRoot
-  const files = globbySync(["**/*.board.tsx", "**/*.circuit.tsx"], {
-    cwd: projectDir,
-    ignore: DEFAULT_IGNORED_PATTERNS,
-  })
-  return {
-    projectDir,
-    circuitFiles: files.map((f) => path.join(projectDir, f)),
-  }
+  return buildFromProjectDir(projectDir)
 }
