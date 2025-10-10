72aea84746f66d7f879bcf56fa5ef0d0caddb75f
Severin Ibarluzea <seveibar@gmail.com>
2025-10-10 12:54:28 -0700

    GLB Loading Examples (reproduction for project-relative-path loading issue) (#525)
    * glb loading path fix
    
    * update props

---
+++ examples/glb-loading/glb-import.tsx
@@ -0,0 +1,20 @@
+// @ts-expect-error tscircuit module is supposed to export this type
+import soicUrl from "./soic8.glb"
+
+export default () => {
+  return (
+    <board width="12mm" height="30mm">
+      <chip
+        name="H1"
+        pcbX={0}
+        pcbY={0}
+        cadModel={<cadmodel modelUrl={soicUrl} />}
+        footprint={
+          <footprint>
+            <hole diameter="0.8mm" pcbX={0} pcbY={0} />
+          </footprint>
+        }
+      />
+    </board>
+  )
+}
