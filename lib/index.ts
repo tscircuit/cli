db58ac46a21387b6728e448a3367b8161bf5ecf2
seveibar <seveibar@gmail.com>
2026-01-30 10:13:14 -0800

    enable external generation of pcm/kicad library for better testing in circuit-json-to-kicad

--- lib/index.ts
+++ lib/index.ts
@@ -1,2 +1,18 @@
 export { createHttpServer } from "./server/createHttpServer"
 export { getLocalFileDependencies } from "./dependency-analysis/getLocalFileDependencies"
+
+// KiCad library conversion exports
+export {
+  convertToKicadLibrary,
+  type ConvertToKicadLibraryOptions,
+  type CircuitJsonToKicadModule,
+} from "./shared/convert-to-kicad-library"
+export {
+  buildKicadPcm,
+  type BuildKicadPcmOptions,
+} from "../cli/build/build-kicad-pcm"
+export {
+  generatePcmAssets,
+  type GeneratePcmAssetsOptions,
+  type GeneratePcmAssetsResult,
+} from "./shared/generate-pcm-assets"
