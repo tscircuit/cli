316d2f764083fba9b55725421bb6e53e96e4d7e3
Arnav K <arnavkaushal09@gmail.com>
2026-02-15 01:37:04 +0530

    Add viewport meta tag to HTML file

--- lib/site/getStaticIndexHtmlFile.ts
+++ lib/site/getStaticIndexHtmlFile.ts
@@ -36,6 +36,7 @@ export const getStaticIndexHtmlFile = ({
       <link rel="icon" type="image/png" href="https://github.com/tscircuit.png">
       <meta charset="UTF-8" />
       <title>tscircuit</title>
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
     </head>
     <body>
 ${scriptBlock}      <script src="https://cdn.tailwindcss.com"></script>
