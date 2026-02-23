8438769f18c2e7228ad7d0b2cd69dda4ebfbba7a
Severin Ibarluzea <seve700@gmail.com>
2026-02-22 18:37:18 -0800

    Handle non-interactive CLI mode via tty flag/env

--- lib/utils/should-be-interactive.ts
+++ lib/utils/should-be-interactive.ts
@@ -3,5 +3,11 @@ export const shouldBeInteractive = () => {
   if (process.env.NODE_ENV === "ci") return false
   if (process.env.CI) return false
   if (process.env.TSCI_TEST_MODE) return false
+
+  if (process.argv.includes("--non-interactive")) return false
+  if (process.env.TSCIRCUIT_NON_INTERACTIVE === "1") return false
+
+  if (!process.stdin.isTTY || !process.stdout.isTTY) return false
+
   return true
 }
