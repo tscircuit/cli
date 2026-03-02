b16ec5435ba0aa73f4bfb6c4f437588e9e6e9771
Arnav K <arnavkaushal09@gmail.com>
2026-03-02 15:25:01 +0530

    fddfdfd

--- cli/init/register.ts
+++ cli/init/register.ts
@@ -113,9 +113,7 @@ export default () => (
         )
 
         if (saveProjectConfig(null, projectDir)) {
-          console.log(
-            "Created tscircuit.config.json with schema",
-          )
+          console.log("Created tscircuit.config.json with schema")
         }
 
         writeFileIfNotExists(
