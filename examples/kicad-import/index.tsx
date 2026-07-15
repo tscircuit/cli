5865276571796152b291a047e451045bd6242bd4
Manish chaudhary <125847751+techmannih@users.noreply.github.com>
2026-07-16 00:11:33 +0530

    Add kicad_sym example (#3714)

--- examples/kicad-import/index.tsx
+++ examples/kicad-import/index.tsx
@@ -1,9 +1,10 @@
 import kicadMod from "./footprint.kicad_mod"
+import kicadSymbol from "./symbol.kicad_sym"
 
 export default () => {
   return (
     <board>
-      <chip footprint={kicadMod} name="U1" />
+      <chip footprint={kicadMod} name="U1" symbol={kicadSymbol} />
     </board>
   )
 }
